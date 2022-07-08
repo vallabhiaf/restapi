ac74fadb4eec5f1c84089f61edbb8c9bf41f239d
Vallabh Chugh <vallabh.chugh@sap.com>
2022-07-08 15:45:04 +0530

    Exception Handling

--- src/main/java/com/dev/restv1/restapi/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/dev/restv1/restapi/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,17 @@
 package com.dev.restv1.restapi.exception;
 
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.dev.restv1.restapi.user.UserNotFoundException;
 
 //Extending default exception handling provided
 //caling it a restcontroller as it provides a result back
@@ -10,5 +19,25 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
-
+	
+	
+    // We need to override this method for specific exceptions which was predefined in ResponseEntityExceptionHandler
+	
+	//Whenever a exception happens this method to be called.
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+		//additonal info ( request.getdesxription(false )has to be decided on case by case basis 
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);}
+		
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) throws Exception {
+		//additonal info ( request.getdesxription(false )has to be decided on case by case basis 
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
+			
+		  return new ResponseEntity(exceptionResponse,HttpStatus.NOT_FOUND);
+		
+	
+}
 }
